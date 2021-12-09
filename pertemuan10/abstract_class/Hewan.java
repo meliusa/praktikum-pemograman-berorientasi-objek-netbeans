348e9cef3451da66394d9aecbd1009e455f1c112
meliusa <78891873+meliusa@users.noreply.github.com>
2021-12-09 15:50:08 +0700

    Add files via upload

---
+++ pertemuan10/abstract_class/Hewan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan10.abstract_class;
+
+/**
+ *
+ * @author Asus
+ */
+public abstract class Hewan {
+
+    private int umur;
+
+    protected Hewan() {
+        this.umur = 0;
+    }
+
+    public void bertambahUmur() {
+        this.umur += 1;
+    }
+
+    public abstract void bergerak();
+
+}
